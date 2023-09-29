ac8f9496660865405dd14d5745cd9881f70ae7fb
hp1heloisa <heloisapimentel2202@gmail.com>
2023-09-28 21:56:33 -0300

    404 sem hotel

--- src/services/hotels-service.ts
+++ src/services/hotels-service.ts
@@ -2,8 +2,8 @@ import { notFoundError } from "@/errors";
 import { hotelsRepository } from "@/repositories/hotels-repository";
 
 async function getHotels(){
-    const allHotels = await hotelsRepository.getHotels()
-    if (allHotels.length == 0) throw notFoundError;
+    const allHotels = await hotelsRepository.getHotels();
+    if (allHotels.length == 0) throw notFoundError();
     return allHotels;
 }
 
